c40efa445086dc6f20882ad0c401aaa3ea8c0b10
Anthony Chung <anthonychung14@gmail.com>
2016-01-06 20:40:49 -0800

    create factory and post complete

--- client/factories/factories.js
+++ client/factories/factories.js
@@ -1,1 +1,18 @@
 angular.module('factories', [])
+
+.factory('Meals', function($http) {
+  var storeMeal = function(meal) {
+    return $http({
+      method: 'POST',
+      url: '/api/create',
+      data: meal
+    })
+    .then(function(resp) {
+      return resp.data
+    })
+  }
+
+  return {
+    storeMeal: storeMeal
+  }
+})
