ae35a81a192be2cdd745b683b2ab4c1e64847d45
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-01-06 20:05:37 -0800

    Created meal and user controllers with functions to instantiate meals and users

---
+++ userController.js
@@ -0,0 +1,26 @@
+var Q = require('q');
+var db = require('./dbconfig.js');
+var User = require('./user.js');
+
+//findone is the actual mongoose method, and it is being called on the User model provided as the second arg. 
+var findUser = Q.nbind(User.findOne, User);
+// create user is a method that uses the create mongoose method to instantiate a new User model
+var createUser = Q.nbind(User.create, User);
+
+//export methods that utilize the Q methods above. 
+module.exports = {
+  create: function(req, res, next){
+    findMeal({title : req.body.title})
+	  .then(function (user){
+	    if (user){
+	      //should refactor this later not to return an error but to check the session. 
+		  next(new Error('Meal is already in Mongo database'));
+		} else {
+	      return createUser({
+		    username: req.body.username,
+		    password: req.body.password
+		  });
+		}
+	  })
+  }
+}
