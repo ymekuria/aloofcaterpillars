d7f6d90e99b9a85927e83f1b50c0f25fac510b90
jfiamor <jonathanfiamor@gmail.com>
2016-01-06 20:07:11 -0800

    added a few forms in browse

--- client/browse/browse.js
+++ client/browse/browse.js
@@ -1,3 +1,26 @@
-angular.module('browse', [])
+angular
+  .module('MyApp',['ngMaterial', 'ngMessages'])
+  .controller('DemoCtrl', function($scope) {
+    $scope.user = {
+      firstName: '',
+      lastName: '',
+    };
 
-.controller('browseCtrl', function() {})
+    $scope.toppings = [
+      { category: 'meat', name: 'Chichen' },
+      { category: 'meat', name: 'Beef' },
+      { category: 'meat', name: 'Pork' },
+      { category: 'meat', name: 'Bacon' },
+      { category: 'veg', name: 'Tofu' },
+      { category: 'veg', name: 'Beans' },
+      { category: 'veg', name: 'Protein Shake' },
+      { category: 'veg', name: 'Grass' }
+    ];
+  })
+  .config(function($mdThemingProvider) {
+
+    $mdThemingProvider.theme('docs-dark', 'default')
+      .primaryPalette('yellow')
+      .dark();
+
+  });
