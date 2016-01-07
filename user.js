01f1c5dc43aa3ef99a50564e2594e622bf8c5449
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-01-06 16:43:34 -0800

    Added user file which includes user schema and will export user model

---
+++ user.js
@@ -0,0 +1,14 @@
+//this file contains the database schema for our users. Here we export the instantiated user model
+
+//must require the database connection from config.js
+var db = require('./config');
+//must also require mongoose
+var mongoose = require('mongoose');
+
+//this variable creates the schema for our users. It includes a name, a password, and a meal object. 
+var userSchema = mongoose.schema({
+  name: String,
+  password: String,
+  meal: Object
+});
+
