acf111f4716728362ade68c5a0f2176cf4bb5407
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-01-06 16:33:44 -0800

    Added comments to confij.js to improve documentation

--- config.js
+++ config.js
@@ -1,14 +1,19 @@
 var mongoose = require('mongoose');
 
+//create mongoose database connection with name database
 mongoose.connect('mongodb://localhost/database');
 
+//save connection in variable db
 var db = mongoose.connection;
 
+//set up listener for errors if unable to connect to server
 db.on('error', console.error.bind(console, 'CONNECTION ERROR'));
 
+//set up listener for success to know that connection was successful
 db.once('open', function() {
   console.log('The database is connected'); 
 }); 
 
+//export db
 module.exports = db;
 
