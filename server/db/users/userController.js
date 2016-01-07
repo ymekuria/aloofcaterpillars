e63d2de26d710bb8bb499d150159f0c7e2908b12
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-01-07 09:22:26 -0800

    Updated user and meal controllers - meal controller now has methods for find, show all and create

--- server/db/users/userController.js
+++ server/db/users/userController.js
@@ -10,11 +10,11 @@ var createUser = Q.nbind(User.create, User);
 //export methods that utilize the Q methods above. 
 module.exports = {
   create: function(req, res, next){
-    findMeal({title : req.body.title})
+    findUser({title : req.body.title})
 	  .then(function (user){
 	    if (user){
 	      //should refactor this later not to return an error but to check the session. 
-		  next(new Error('Meal is already in Mongo database'));
+		  next(new Error('User is already in Mongo database'));
 		} else {
 	      return createUser({
 		    username: req.body.username,
