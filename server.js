bda284fd43c46034bd2f4b2cefc005dff88b3fcf
Hack Reactor Students <accounts@hackreactor.com>
2016-01-06 16:19:32 -0800

    added a basic server

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var express = require('express');
+var partials = require('express-partials');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.listen(5000);
