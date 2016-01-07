802283660b1d20705595a7e2c0bb082051299ad3
Anthony Chung <anthonychung14@gmail.com>
2016-01-06 20:56:01 -0800

    next commit. post from createfoods returns404

--- server-routes/routes.js
+++ server-routes/routes.js
@@ -1,31 +1,31 @@
 var app = require('../server/server-config');
 
 // Auth
-app.get('/api/signin', function (res,resp){
+// app.get('/api/signin', function (res,resp){
 
-});
+// });
 
-app.post('/api/signin', function (res, resp){
+// app.post('/api/signin', function (res, resp){
 
-});
+// });
 
-// browse
-app.get('/api/browse', function (res,resp){
+// // browse
+// app.get('/api/browse', function (res,resp){
 
-});
+// });
 
-app.post('/api/browse', function (res, resp){
+// app.post('/api/browse', function (res, resp){
 
-});
+// });
 
-// create
-app.get('/api/create', function (res,resp){
+// // create
+// app.get('/api/create', function (res,resp){
 
-});
+// });
 
-app.post('/ap/create', function (res, resp){
+// app.post('/ap/create', function (res, resp){
 
-});
+// });
 
 
 
