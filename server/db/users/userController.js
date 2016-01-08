43d078504d79c13d99f305b35e2371f926740217
Hack Reactor Students <accounts@hackreactor.com>
2016-01-07 19:42:17 -0800

    Removed comments

--- server/db/users/userController.js
+++ server/db/users/userController.js
@@ -78,32 +78,7 @@ module.exports = {
         res.redirect('/browse');
         res.json({token: token});
       }); 
-    
 
-         // var username = req.body.username;
-    // var password = req.body.password;
-
-    // // check to see if user already exists
-    // findUser({username: username})
-    //   .then(function (user) {
-    //     if (user) {
-    //       next(new Error('User already exist!'));
-    //     } else {
-    //       // make a new user if not one
-    //       return createUser({
-    //         username: username,
-    //         password: password
-    //       });
-    //     }
-    //   })
-    //   .then(function (user) {
-    //     // create token to send back for auth
-    //     var token = jwt.encode(user, 'secret');
-    //     res.json({token: token});
-    //   })
-    //   .fail(function (error) {
-    //     next(error);
-    //   });
   }
 
 };
