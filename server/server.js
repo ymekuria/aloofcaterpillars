8fa5db27501204b8c76b8803ea4810e1e4b75f6f
Yoni <ymekuria@gmail.com>
2016-01-11 15:17:38 -0800

    updated server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 5000;
 var server = app.listen(port, function(){
   var host = server.address().address;
   var port = server.address().port;
-  console.log('One2One server listening on: ', host, port);
+  console.log('One2One server listening here on: ', host, port);
 });
 
 module.exports = app;
